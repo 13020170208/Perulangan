d6c1aa7cd4a4e5149774409b737f742aa766e9f1
13020170208 <68625050+13020170208@users.noreply.github.com>
2020-08-10 03:31:20 +0800

    Add files via upload

---
+++ ContohPerulangan/src/Perulangan/ContohDoWhile.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Perulangan;
+
+/**
+ *
+ * @author AfdalMaulana
+ */
+public class ContohDoWhile {
+    public static void main(String[] args) {
+        int data = 0;
+        do{
+            System.out.println("Data : "+data);
+            data++;
+        }while(data<=10);
+    }
+}
---
+++ ContohPerulangan/src/Perulangan/ContohWhile.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Perulangan;
+
+/**
+ *
+ * @author AfdalMaulana
+ */
+public class ContohWhile {
+    public static void main(String[] args) {
+        int data = 0;
+        while(data<=10){
+            System.out.println("Data : "+data);
+            data++;
+        }
+    }
+}
